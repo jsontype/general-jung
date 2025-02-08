82e5db03d374c41a22c6762c7dec7c9f67154651
jsontype <jsontyper@gmail.com>
2025-02-08 14:26:21 +0900

    movie init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import MovieList from './components/MovieList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+      .then(res => res.json())
+      .then(json => { setMovies(json.data.movies) })
+  }, [])
+
+  console.log(movies)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>무비 리스트</h1>
+      <MovieList movies={movies} />
+    </div>
   )
 }
 
